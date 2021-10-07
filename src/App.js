60e624d97b48852e13af95b11b93d0de9168ff5c
juan rojas <jrojasbf@gmail.com>
2021-10-06 17:44:46 -0700

    added header and footer, navbar and react boostrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header />
+
+      <Switch>
+
+        <Route path="/" exact>
+          <h2> Home</h2>
+          <a href="http://localhost:3000/about" >About me</a>
+
+        </Route>
+
+        <Route path="/about">
+
+          <h2> About Me</h2>
+          <a href="http://localhost:3000/">Home</a>
+
+        </Route>
+
+
+      </Switch>
+
+      <Footer />
+    </div >
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+function Header() {
+    return (
+        <Navbar bg="light" >
+            <Container>
+                <Navbar.Brand href="">Juan Rojas</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="about">About Me</Nav.Link>
+                </Nav>
+            </Container>
+
+        </Navbar>
+
+    )
+
+
+}
+
+
+export default Header
