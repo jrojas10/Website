f94c9bf65799d8e83c6b2f255f8e14fb729edd0d
juan rojas <jrojasbf@gmail.com>
2021-10-15 17:18:41 -0700

    intro with picture

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import Footer from "./components/Footer/Footer";
 import Container from 'react-bootstrap/Container'
 import Hero from './components/Hero/Hero'
 import Projects from './components/Projects/Projects';
+import Body from './components/body';
 
 function App() {
 
   return (
+
+
+
+
+
+
+
     <div className="App">
       <Header />
-      <div className="sections">
+
+
+      <Body/>
+      {/* <div className="sections">
         <Hero />
         <Projects />
-      </div>
+      </div> */}
       <Footer />
 
     </div >
