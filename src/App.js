2e3d98728ea3e5eb04be6d55c7ad7b6e4f6e3d30
juan rojas <jrojasbf@gmail.com>
2021-10-17 13:30:07 -0700

    finished body and about section with filler text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Footer from "./components/Footer/Footer";
-import Container from 'react-bootstrap/Container'
-import Hero from './components/Hero/Hero'
-import Projects from './components/Projects/Projects';
 import Body from './components/body';
+import About from './components/About/About';
 
 function App() {
 
@@ -21,6 +19,7 @@ function App() {
 
 
       <Body/>
+      <About/>
       {/* <div className="sections">
         <Hero />
         <Projects />
--- src/components/body.js
+++ src/components/body.js
@@ -1,5 +1,6 @@
-import "./body.scss"
-import Me from "../assets/images/me.jpg"
+import "./body.scss";
+import Me from "../assets/images/me.jpg";
+
 
 function Body() {
 
@@ -19,20 +20,18 @@ function Body() {
                             <div className="i-title-item">num 5</div>    
                         </div>
                     </div>
-                    <div className = "i-description">
+                    <p className = "i-description">
                         I have been working with computers almost my whole life. My goal is to gain experience in a
 computer science related field and be able to learn under others. I hope to use this
 experience gained to further improve my skills and help others in need. I have worked in a
 team environment as a team leader so I know the importance of communication with others. I
 am always open to feedback on my current performance.
-                    </div>
+                    </p>
                 </div>
             </div>
             <div className = "i-right">
                 <div className = "i-bg">
-
-                
-                <img src ={Me} className = "i-img" />
+                <img src ={Me} className = "i-img" alt= "j" />
                 </div>
             </div>
           
