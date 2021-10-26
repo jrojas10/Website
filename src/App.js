4e91efde4982ea02dec779e86e8f6b951991f48c
juan rojas <jrojasbf@gmail.com>
2021-10-25 18:48:57 -0700

    updated names and added scss for header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer/Footer";
 import Body from './components/body';
 import About from './components/About/About';
 import ProjectList from './components/Projects/ProjectList';
-import Contact from './components/Conact/Contact';
+import Contact from './components/Contact/Contact';
 
 function App() {
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
+import "./Header.scss"
+
 
 function Header() {
     return (
         <Navbar bg="dark" variant="dark" sticky="top">
             <Container>
-                <Navbar.Brand href="#Home">Home</Navbar.Brand>
+                <Navbar.Brand href="/">Home</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#About">About</Nav.Link>
                     <Nav.Link href="#Projects">Projects</Nav.Link>
