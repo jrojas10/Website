625c8653a1c0da1326a5069a8ffc5daf511c052d
juan rojas <jrojasbf@gmail.com>
2021-11-08 15:13:41 -0800

    added validation to contact page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../src/components/Contact/Contact.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../src/components/Projects/Projects.scss';
+// import '../src/components/About/About.scss';
+
+// import '../src/components/Header/Header.scss';
+// import '../src/components/Projects/Card.scss';
+// import '../src/components/Footer/Footer.scss';
+// import '../src/components/Intro/Intro.scss';
+
 import { BrowserRouter } from 'react-router-dom';
 
 
