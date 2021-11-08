51a1bd45b4ef2f360528188fa90b9df58516e2bf
juan rojas <jrojasbf@gmail.com>
2021-11-08 09:23:45 -0800

    updated the projects list

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,37 +1,43 @@
 import walmart from "../assets/images/Walmart.jpg"
+import statefarm from "../assets/images/statefarm.png"
 
 
 
 export const projects = [
     {
         id:1,
-        name: "Walmart",
+        name: "Walmart Store Sales Forecasting",
         image: walmart,
-        link : "https://github.com/jesus-r-mendoza/Walmart-Store-Sales-Forecasting"
+        link : "https://github.com/jesus-r-mendoza/Walmart-Store-Sales-Forecasting",
+        description:"Using various Machine Learning algorithms to predict Walmart's sales per week using over 420k publicly available sales records from Walmart"
     },
     {
         id:2,
-        name: "Walmart",
-        image: walmart,
-        link : "https://github.com/jesus-r-mendoza/Walmart-Store-Sales-Forecasting"
+        name: "Statefarm Distracted Driver Detection",
+        image: statefarm,
+        link : "https://github.com/jesus-r-mendoza/State-Farm-Distracted-Driver-Detection",
+        description:"Using more advanced Machine Learning algorithms to classify over 75k unlabeled images into categories based on which action the driver is being distracted with"
     },
     {
         id:3,
-        name: "Walmart",
+        name: "Personal React Website",
         image: walmart,
-        link : "https://github.com/jesus-r-mendoza/Walmart-Store-Sales-Forecasting"
+        link : "https://github.com/jrojas10/Website",
+        description:"This website is a personal project that I built using React.js and deployed on Github Pages"
     },
      {
         id:4,
-        name: "Walmart",
+        name: "Discord Bot",
         image: walmart,
-        link : "https://github.com/jesus-r-mendoza/Walmart-Store-Sales-Forecasting"
+        link : "https://github.com/jrojas10/Mecha-Senku",
+        description:"A simple Discord bot that uses the Discord API to send messages to a channel based on the user's input"
     },
      {
         id:5,
-        name: "Walmart",
+        name: "SEL- Student Exchange Library",
         image: walmart,
-        link : "https://github.com/jesus-r-mendoza/Walmart-Store-Sales-Forecasting"
+        link : "https://github.com/jrojas10/SEL-Student-Exchange-Library",
+        description:"A mockup website that allows students to exchange books with other students built in Java"
     }
 ]
 
