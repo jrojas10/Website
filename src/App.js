6f41b37ec72c2596b4ae609a62cff456d642b1b6
juan rojas <jrojasbf@gmail.com>
2021-11-10 18:44:14 -0800

    added fadeIn component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,38 @@ import About from './components/About/About';
 import ProjectList from './components/Projects/ProjectList';
 import Contact from './components/Contact/Contact';
 
+
+
+import FadeIn from './examples/FadeIn';
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
 
   return (
     <div className="App">
+
+      <Switch>
+
+        <Route exact path="/">
       <Header />
       <Intro/>
       <About/>
+      <FadeIn>
       <ProjectList/>
+      </FadeIn>
       <Contact/>
       <Footer />
+        </Route>
+
+
+        {/* fade test */}
+        <Route path="/test">
+          <Header />
+          
+          <Footer/>
+        </Route>
+
+      </Switch>
     </div >
   );
 }
